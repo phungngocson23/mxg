c48c15988cc81e43b95193950decfb59da7fcb3c
Son Phung Ngoc <son.phungngoc@nttdata.com>
2018-04-27 15:39:37 +0700

    Add mongoose to save

--- api/models/product.js
+++ api/models/product.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 
 const productSchema = mongoose.Schema({
-    name : String,
-    brand : String
-});
+    name: { type: String, require: true, unique: true },
+    brand: String
+}, { versionKey: false });
 
 const Product = module.exports = mongoose.model('Product', productSchema, 'product');
--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
-
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
 mongoose.connect('mongodb://127.0.0.1:27017/mxg');
 const db = mongoose.connection;
-
 db.once('open', () => {
     console.log('Connected Sucessfully !');
 })
@@ -17,11 +21,31 @@ const Product = require('./api/models/product');
 app.get('/', (req, res) => {
     Product.find({}, (err, product) => {
         if (err) {
-            console.log("Failed");
+            throw err;
         } else {
-            console.log(product);
+            res.status(200).json(
+                product = product
+            )
         }
     })
 });
+
+app.post('/', (req, res, next) => {
+    console.log(req.body);
+    const product = new Product({ name: req.body.name, brand: req.body.brand });
+    product
+        .save()
+        .then(data => {
+            res.status(200).json(data)
+        })
+        .catch(next);
+});
+
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    res.status(status).send();
+    console.log(err.message);
+})
+
 
 module.exports = app;
